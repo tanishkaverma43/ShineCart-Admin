98a55c27651736c95d3afc9a2c53542df21928a3
Tanishka Verma <tanishkaverma43@gmail.com>
2025-03-30 22:42:42 +0530

    link updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://e-commer-backend-1erh.onrender.com';
 export const currency = '₹';
 
 function App() {
